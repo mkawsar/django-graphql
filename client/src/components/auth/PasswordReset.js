f53e90d8b5527cf7c02353afa956d2c2fbaa3f08
mkawsar <kawsar.diu888@gmail.com>
2019-08-19 21:54:57 +0600

    Created forgot password form

--- client/src/components/auth/PasswordReset.js
+++ client/src/components/auth/PasswordReset.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { NavLink, withRouter } from "react-router-dom";
 import Footer from '../lib/Footer';
 
 class PasswordReset extends Component {
@@ -10,7 +11,7 @@ class PasswordReset extends Component {
 
                         <div className="row">
                             <div className="col-sm-12 features section-description wow fadeIn">
-                                <h2>Bootstrap Navbar Menu</h2>
+                                <h2>Forgot Password</h2>
                                 <div className="divider-1">
                                     <div className="line"></div>
                                 </div>
@@ -18,15 +19,32 @@ class PasswordReset extends Component {
                         </div>
 
                         <div className="row">
-                            <div className="col-sm-6 features-box wow fadeInLeft">
+                            <div className="col-sm-12 features-box wow fadeInLeft">
                                 <div className="row">
-                                    Hello world
+                                    <form id="forgot-password" className="form-horizontal">
+                                        <div className="form-group">
+                                            <label className="control-label col-sm-3">Email</label>
+                                            <div className="col-sm-9">
+                                                <input type="text" name="email" placcholder="Email"
+                                                    className="form-control" required
+                                                    onChange={e => this.setState({ email: e.target.value })} />
+                                            </div>
+                                        </div>
+                                        <div className="form-group">
+                                            <div className="col-sm-3">
+                                            </div>
+                                            <div className="col-sm-9">
+                                                <button className="btn btn-success pull-left">Submit</button>
+                                                <NavLink to="/login">Login</NavLink>
+                                            </div>
+                                        </div>
+                                    </form>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-                <Footer/>
+                <Footer />
             </div>
         )
     }
